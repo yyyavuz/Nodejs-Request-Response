742ceb4b6da18d0741571324654ac8b6f6ad6fae
yyyavuz <yusufyyavuz@outlook.com>
2019-05-15 15:12:34 +0300

    Update mongofile.js

--- mongofile.js
+++ mongofile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
       "db": {
-        "connection": "mongodb://dbUser:dbPassword1@ds249623.mlab.com:49623/getir-case-study",
+        "connection": "mongodb://************************************************",
         "name": "testdb"
       }
   },
